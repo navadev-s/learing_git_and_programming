512ebe2b1fd8f380dfb9bff2a24f7828546adcb1
navadev-s <navadevsunil3132@gmail.com>
2025-12-11 10:57:13 +0530

    Sumof n numbers in an array,commas well placed

---
+++ programs/arraysum.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+	int a[100],n,i,s=0;
+	printf("Enter the array size: ");
+	scanf("%d",&n);
+	printf("Enter the elements :\n");
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+		s=s+a[i];
+	}
+	printf("Entered elements are : ");
+	for(i=0;i<n;i++){
+		printf("%d",a[i]);
+		if(i==n-1){
+			break;
+		}
+		else{
+			printf(",");
+		}	
+		
+		
+		
+	}
+	printf("\n");
+	printf("Sum = %d\n",s);
+}
