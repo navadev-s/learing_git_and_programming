696b4b3e5f5b899644b64951ddb6e690493bdf90
navadev-s <navadevsunil3132@gmail.com>
2025-12-12 17:04:45 +0530

    sum of 2 numbers using java

---
+++ programs/java/Numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Numbers{
+	public static void main(String ar[]){
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter 2 numbers : ");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=a+b;
+		System.out.println("Sum = "+c);
+	}
+}
